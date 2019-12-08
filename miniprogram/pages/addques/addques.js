b752c8a3b2a3678ca0a75d5caa584b7a8c28e09c
chenqixian <chenqixian@buaa.edu.cn>
2019-12-08 13:19:23 +0800

    问答页文字修改

--- miniprogram/pages/addques/addques.js
+++ miniprogram/pages/addques/addques.js
@@ -1,18 +1,22 @@
 var app = getApp();
 Page({
   data: {
-    array: ['美国', '中国', '巴西', '日本'],
-    settingLocation: '中关村768创意产业园',
-    settingTime: '07:00',
-    settingLon: 116.35218,
-    settingLat: 40.0089,
+    contriArray: ['匿名贡献', '先先', '老祁', '枫宝','琦琦','尚纯','亮哥'],
+    ansArray: ['当前学员', '先先', '老祁', '枫宝', '琦琦', '尚纯', '亮哥'],
     userInfo: {},
-    index: 0
+    contriIndex: 0,
+    ansIndex: 0
   },
-  bindPickerChange: function (e) {
+  contriPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      index: e.detail.value
+      contriIndex: e.detail.value
+    })
+  },
+  ansPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      ansIndex: e.detail.value
     })
   },
   /** 确认订阅 */
