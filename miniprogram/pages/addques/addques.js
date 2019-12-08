fdb0ee310d993ad65707ff8a1922133e32441122
chenqixian <chenqixian@buaa.edu.cn>
2019-12-08 16:17:10 +0800

    增加问题输入入口

--- miniprogram/pages/addques/addques.js
+++ miniprogram/pages/addques/addques.js
@@ -1,55 +1,58 @@
 var app = getApp();
+const db = wx.cloud.database();
 Page({
   data: {
     contriArray: ['匿名贡献', '先先', '老祁', '枫宝','琦琦','尚纯','亮哥'],
     ansArray: ['当前学员', '先先', '老祁', '枫宝', '琦琦', '尚纯', '亮哥'],
-    userInfo: {},
+    question: '',
     contriIndex: 0,
     ansIndex: 0
   },
   contriPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       contriIndex: e.detail.value
     })
   },
   ansPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       ansIndex: e.detail.value
     })
   },
-  /** 确认订阅 */
-  confirmSubscription: function () {
-    wx.showToast({
-      title: '订阅成功',
-      icon: 'success',
-      duration: 1500,
-      mask: true
+  inputChange: function(e) {
+    this.setData({
+      question: e.detail.value
     })
   },
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-    app.globalData.firstOpen = false;
-  },
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+  /** 确认上传 */
+  confirmSubscription: function () {
+    if(!this.data.question) {
+      wx.showModal({
+        title: '注意',
+        content: '考核内容不能为空哦！',
+        showCancel: false
+      })
+      return;
+    }
+    wx.showLoading({
+      title: '题目入库中',
+      mask: true
+    })
+    console.log(this.data.question, this.data.contriIndex, this.data.ansIndex);
+    db.collection('questionBase').add({
+      data: {
+        question: this.data.question,
+        contriIndex: this.data.contriIndex,
+        ansIndex: this.data.ansIndex
+      }
+    }).then(res => {
+      wx.hideLoading();
+      wx.showToast({
+        title: '成功入库'
+      })
+    }).catch(err => {
+      wx.showToast({
+        title: '上传失败'
+      })
+    })
   }
 })
