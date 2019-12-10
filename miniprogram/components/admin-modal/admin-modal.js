5e06511b131143d7bf07384bbed3734b03483a96
chenqixian <chenqixian@buaa.edu.cn>
2019-12-10 16:56:42 +0800

    增加管理员登入验证组件

---
+++ miniprogram/components/admin-modal/admin-modal.js
@@ -0,0 +1,58 @@
+// components/login-modal/login-modal.js
+const ROOMNAME = 'ChrisChen'
+const PASSWORD = 'laoqi1215'
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    show: {
+      type: Boolean
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    roomId: '',
+    password: ''
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+    clickMask: function () {
+      this.setData({ show: false })
+    },
+    cancel: function () {
+      this.setData({ show: false })
+    },
+    roomId: function(e) {
+      this.setData({ 
+        roomId: e.detail.value
+      })
+    },
+    roomPwd: function(e) {
+      this.setData({
+        password: e.detail.value
+      })
+    },
+    confirm: function() {
+      if(this.data.roomId == ROOMNAME && this.data.password == PASSWORD) {
+        wx.navigateTo({
+          url: '../../pages/manage/manage',
+        })
+      }
+      else {
+        this.setData({ show: false })
+        wx.showModal({
+          title: '注意',
+          content: '用户名或密码错误',
+          showCancel: false
+        })
+      }
+    }
+  }
+})
